c37100b6b3ee4ecc3481269ed8fc340312aa36ee
Akinola Abiodun Emmanuel <akinolaemmanuel49@gmail.com>
2025-01-06 04:21:21 +0100

    Add missing order routes

--- routes/order_routes.go
+++ routes/order_routes.go
@@ -64,4 +64,59 @@ func RegisterOrderRoutes(config *configs.Config, router *mux.Router, db *mongo.D
 			orderHandler.Delete(w, r, id)
 		}
 	})
+
+	router.HandleFunc(RouteOrders+"/{id}/address", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"] // Extract the `id` path parameter
+
+		switch r.Method {
+		case "PATCH":
+			orderHandler.UpdateOrderShippingAddress(w, r, id)
+		}
+	})
+
+	router.HandleFunc(RouteOrders+"/{id}/status", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"] // Extract the `id` path parameter
+
+		switch r.Method {
+		case "PATCH":
+			orderHandler.UpdateOrderStatus(w, r, id)
+		}
+	})
+
+	router.HandleFunc(RouteOrders+"/{id}/items/add", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"] // Extract the `id` path parameter
+
+		switch r.Method {
+		case "PATCH":
+			orderHandler.AddItemToOrder(w, r, id)
+		}
+	})
+
+	router.HandleFunc(RouteOrders+"/{id}/items/{productId}", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"]               // Extract the `id` path parameter
+		productId := mux.Vars(r)["productId"] // Extract the `productId` path parameter
+
+		switch r.Method {
+		case "PATCH":
+			orderHandler.RemoveItemFromOrder(w, r, id, productId)
+		}
+	})
+
+	router.HandleFunc(RouteOrders+"/{id}/confirm", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"] // Extract the `id` path parameter
+
+		switch r.Method {
+		case "PATCH":
+			orderHandler.ConfirmOrder(w, r, id)
+		}
+	})
+
+	router.HandleFunc(RouteOrders+"/{id}/cancel", func(w http.ResponseWriter, r *http.Request) {
+		id := mux.Vars(r)["id"] // Extract the `id` path parameter
+
+		switch r.Method {
+		case "PATCH":
+			orderHandler.CancelOrder(w, r, id)
+		}
+	})
 }
